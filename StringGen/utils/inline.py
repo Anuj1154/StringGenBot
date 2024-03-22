eeb8b2b26062f5b3e4b04a7e38691b056b2dec7b
Anuj1154 <158982341+Anuj1154@users.noreply.github.com>
2024-03-23 02:11:05 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -5,11 +5,11 @@ from config import SUPPORT_CHAT
 
 keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="gensession")],
+        [InlineKeyboardButton(text="𝐒𝐓𝐑𝐈𝐍𝐆 𝐒𝐄𝐒𝐒𝐈𝐎𝐍", callback_data="gensession")],
         [
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
+            InlineKeyboardButton(text="𝐌𝐎𝐍𝐒𝐓𝐄𝐑", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="𝐂𝐑𝐄𝐀𝐓𝐎𝐑", url="https://t.me/monster_king_is_here"
             ),
         ],
     ]
@@ -18,13 +18,13 @@ keyboard = InlineKeyboardMarkup(
 gen_key = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v1", callback_data="pyrogram1"),
-            InlineKeyboardButton(text="ᴩʏʀᴏɢʀᴀᴍ v2", callback_data="pyrogram"),
+            InlineKeyboardButton(text="𝐏𝐑𝐎𝐆𝐑𝐀𝐌 𝐕𝟏", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="𝐏𝐑𝐎𝐆𝐑𝐀𝐌 𝐕𝟐", callback_data="pyrogram"),
         ],
-        [InlineKeyboardButton(text="ᴛᴇʟᴇᴛʜᴏɴ", callback_data="telethon")],
+        [InlineKeyboardButton(text="𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍", callback_data="telethon")],
     ]
 )
 
 retry_key = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+    [[InlineKeyboardButton(text="𝐓𝐑𝐘 𝐀𝐆𝐀𝐈𝐍", callback_data="gensession")]]
 )
